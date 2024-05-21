248b02097a71f899116cbc541d0b2194ef7826e7
Sasiba0 <balintsasvari@gmail.com>
2024-05-21 21:06:28 +0200

    új form elmek

---
+++ Kliensalkalmazas_AzMintTavaly/UtazasEditForm.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Kliensalkalmazas_AzMintTavaly
+{
+    partial class UtazasEditForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.URLSlug_TextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nev_TextBox = new System.Windows.Forms.TextBox();
+            this.ar_numericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.leiras_listBox = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ar_numericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // URLSlug_TextBox
+            // 
+            this.URLSlug_TextBox.Location = new System.Drawing.Point(39, 175);
+            this.URLSlug_TextBox.Name = "URLSlug_TextBox";
+            this.URLSlug_TextBox.Size = new System.Drawing.Size(100, 20);
+            this.URLSlug_TextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 159);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "URL Slug";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Név";
+            // 
+            // nev_TextBox
+            // 
+            this.nev_TextBox.Location = new System.Drawing.Point(39, 107);
+            this.nev_TextBox.Name = "nev_TextBox";
+            this.nev_TextBox.Size = new System.Drawing.Size(100, 20);
+            this.nev_TextBox.TabIndex = 2;
+            // 
+            // ar_numericUpDown
+            // 
+            this.ar_numericUpDown.Location = new System.Drawing.Point(237, 108);
+            this.ar_numericUpDown.Maximum = new decimal(new int[] {
+            999999999,
+            0,
+            0,
+            0});
+            this.ar_numericUpDown.Name = "ar_numericUpDown";
+            this.ar_numericUpDown.Size = new System.Drawing.Size(120, 20);
+            this.ar_numericUpDown.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(234, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Ár";
+            // 
+            // leiras_listBox
+            // 
+            this.leiras_listBox.FormattingEnabled = true;
+            this.leiras_listBox.Location = new System.Drawing.Point(39, 235);
+            this.leiras_listBox.Name = "leiras_listBox";
+            this.leiras_listBox.Size = new System.Drawing.Size(120, 95);
+            this.leiras_listBox.TabIndex = 6;
+            // 
+            // UtazasEditForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.leiras_listBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ar_numericUpDown);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nev_TextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.URLSlug_TextBox);
+            this.Name = "UtazasEditForm";
+            this.Text = "UtazasEditForm";
+            ((System.ComponentModel.ISupportInitialize)(this.ar_numericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox URLSlug_TextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox nev_TextBox;
+        private System.Windows.Forms.NumericUpDown ar_numericUpDown;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox leiras_listBox;
+    }
+}
---
+++ Kliensalkalmazas_AzMintTavaly/UtazasEditForm.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Hotcakes.CommerceDTO;
+using Hotcakes.CommerceDTO.v1;
+using Hotcakes.CommerceDTO.v1.Catalog;
+using Hotcakes.CommerceDTO.v1.Client;
+using Newtonsoft.Json;
+
+namespace Kliensalkalmazas_AzMintTavaly
+{
+    public partial class UtazasEditForm : Form
+    {
+        int eredetiar;
+        string leiras, sku, nev;
+        public UtazasEditForm()
+        {
+            InitializeComponent();
+        }
+
+
+        public UtazasEditForm(ProductDTO termek)
+        {
+            InitializeComponent();
+            string OG_JSON = JsonConvert.SerializeObject(termek);
+            string[] tordelt = OG_JSON.Split(',');
+            for (int i = 0; i < tordelt.Length; i++)
+            {
+
+                if (tordelt[i].Contains("\"SitePrice\":"))
+                {
+                    //MessageBox.Show(tordelt[i].Split(':')[1].Split('.')[0]);
+                    ar_numericUpDown.Value = Convert.ToInt32(tordelt[i].Split(':')[1].Split('.')[0].Trim());
+                    //tordelt[i] = "\"SitePrice\":542000";
+                }
+
+                if (tordelt[i].Contains("\"ProductName\":"))
+                {
+                    nev_TextBox.Text =tordelt[i].Split(':')[1];
+                }
+
+                if (tordelt[i].Contains("\"UrlSlug\":"))
+                {
+                    URLSlug_TextBox.Text = tordelt[i].Split(':')[1];
+                }
+
+                if (tordelt[i].Contains("\"LongDescription\":"))
+                {
+                    leiras_listBox.Text = tordelt[i].Split(':')[1];
+                }
+            }
+            string uj_JSON = String.Join(",", tordelt);
+            ProductDTO frissTermek = JsonConvert.DeserializeObject<ProductDTO>(uj_JSON);
+            Form1.aktualisTermek = frissTermek;
+            return;
+        }
+    }
+}
